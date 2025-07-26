3fc5a2dc0c9fccb1e111c671dc3081b0c575e48e
Vishesh <24je0718@iitism.ac.in>
2025-07-26 18:38:08 +0530

    defined models fields

---
+++ backend/models/Messages/MessageModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+const MessageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      id: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+      type: String,
+      required: true,
+    },
+    receiverId: {
+      id: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    imageUrl: {
+      type: String,
+      default: "",
+    },
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+const MessageModel =
+  mongoose.models.message || mongoose.model("message", MessageSchema);
+export default MessageModel;
